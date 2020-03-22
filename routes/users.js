c3b84515b13c8049a600c33925b202daad547292
Selman K <selmankahya@hotmail.com>
2020-03-22 14:12:37 -0700

    update score

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,18 @@ const users = [];
 /* GET users listing. */
 router.get("/", function(req, res, next) {
   res.send(
-    users.sort((user1, user2) => user1.score <= user2.score).slice(0, 10)
+    users
+      .sort((user1, user2) => {
+        if (user1.score < user2.score) {
+          return 1;
+        }
+        if (user1.score > user2.score) {
+          return -1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+      .slice(0, 10)
   );
 });
 
